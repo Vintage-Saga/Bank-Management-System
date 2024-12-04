ccee7d10ae5057ce84d25bdb6dd34f4310e2cdbb
Kartikey <vintagesaga26@gmail.com>
2024-12-04 23:30:18 +0530

    initial commit

--- src/main/java/com/bank/dao/Bankmandao.java
+++ src/main/java/com/bank/dao/Bankmandao.java
@@ -5,152 +5,58 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
-import java.util.ArrayList;
 
 public class Bankmandao {
-    
-    private String jdbcURL = "jdbc:mysql://localhost:30006/BankMan";
-    private String jdbcid = "root";
-    private String jdbcname = "root";
-    
-    private static final String INSERT_USER_SQL = "INSERT INTO Bank (id, name, dob, balance) VALUES (?, ?, ?, ?);";
-    private static final String SELECT_USER_BY_ID = "SELECT * FROM Bank WHERE id=?;";
-    private static final String SELECT_ALL_USERS = "SELECT * FROM Bank;";
-    private static final String DELETE_USERS_SQL = "DELETE FROM Bank WHERE id=?;";
-    private static final String UPDATE_USERS_SQL = "UPDATE Bank SET id=?, name=?, dob=?, balance=? WHERE id=?;";
-    
-    public Bankmandao() {
-        super();
-    }
-    
-    public Connection getConnection() {
+    private String jdbcURL = "jdbc:mysql://localhost:3306/bankdb"; // Update with your database URL
+    private String jdbcUsername = "root"; // Update with your database username
+    private String jdbcPassword = "password"; // Update with your database password
+
+    private static final String INSERT_ACCOUNT_SQL = "INSERT INTO accounts (account_number, account_holder, balance) VALUES (?, ?, ?);";
+    private static final String SELECT_ACCOUNT_BY_NUMBER = "SELECT * FROM accounts WHERE account_number = ?;";
+    private static final String SELECT_ALL_ACCOUNTS = "SELECT * FROM accounts;";
+    private static final String DELETE_ACCOUNT_SQL = "DELETE FROM accounts WHERE account_number = ?;";
+    private static final String UPDATE_ACCOUNT_SQL = "UPDATE accounts SET account_holder = ?, balance = ? WHERE account_number = ?;";
+
+    public Bankmandao() {}
+
+    protected Connection getConnection() {
         Connection connection = null;
-        
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(jdbcURL, jdbcid, jdbcname);
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
+            connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return connection;
     }
-    
-    public void insertUser (BankMan user) {
-        try (Connection connection = getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USER_SQL);
-            preparedStatement.setString(1, user.getId());
-            preparedStatement.setString(2, user.getName());
-            preparedStatement.setString(3, user.getDob());
-            preparedStatement.setString(4, user.getBalance());
-            
+
+    public void insertAccount(Account account) {
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_ACCOUNT_SQL)) {
+            preparedStatement.setString(1, account.getAccountNumber());
+            preparedStatement.setString(2, account.getAccountHolder());
+            preparedStatement.setDouble(3, account.getBalance());
             preparedStatement.executeUpdate();
-        } catch (Exception e) { 
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public BankMan selectUser (int id) {
-        BankMan user = null; // Start with null
-        try (Connection connection = getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_USER_BY_ID); 
-            preparedStatement.setInt(1, id);
-            
-            ResultSet resultSet = preparedStatement.executeQuery();
-            
-            if (resultSet.next()) {
-                user = new BankMan(resultSet.getString("id"), resultSet.getString("name"), resultSet.getString("dob"), resultSet.getString("balance"));
+    public Account selectAccount(String accountNumber) {
+        Account account = null;
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ACCOUNT_BY_NUMBER)) {
+            preparedStatement.setString(1, accountNumber);
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()) {
+                String accountHolder = rs.getString("account_holder");
+                double balance = rs.getDouble("balance");
+                account = new Account (accountNumber, accountHolder, balance);
             }
-        } catch (Exception e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-        
-        return user; // Return null if no user is found
-    }
-    
-    public List<BankMan> selectAllBankMan() {
-        List<BankMan> users = new ArrayList<>();
-        
-        try (Connection connection = getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_USERS);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            
-            while (resultSet.next()) {
-                String id = resultSet.getString("id");
-                String name = resultSet.getString("name");
-                String dob = resultSet.getString("dob");
-                String balance = resultSet.getString("balance");
-                
-                users.add(new BankMan(id, name, dob, balance));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        
-        return users;
-    }
-    
-    public boolean deleteUser (int id) {
-        boolean status = false;
-        
-        try (Connection connection = getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement(DELETE_USERS_SQL);
-            preparedStatement.setInt(1, id);
-            
-            status = preparedStatement.executeUpdate() > 0; // returns true if a row was affected
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return status;
-    }
-}
-
-// Ensure BankMan has the appropriate constructor
-class BankMan {
-    private String id;
-    private String name;
-    private String dob;
-    private String balance;
-
-    public BankMan(String id, String name, String dob, String balance) {
-        this.id = id;
-        this.name = name;
-        this.dob = dob;
-        this.balance = balance;
+        return account;
     }
 
-    // Getters and Setters
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDob() {
-        return dob;
-    }
-
-    public void setDob(String dob) {
-        this.dob = dob;
-    }
-
-    public String getBalance() {
-        return balance;
-    }
-
-    public void setBalance(String balance) {
-        this.balance = balance;
-    }
+    // Implement other methods like selectAllAccounts, deleteAccount, updateAccount similarly
 }
